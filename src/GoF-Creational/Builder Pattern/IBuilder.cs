01b8f7d851eef4bf6f194035989d7d6d280bcf4f
shreesharao <sshreesha4@gmail.com>
2018-04-18 17:26:59 +0530

    Patterns project initial commit

---
+++ src/GoF-Creational/Builder Pattern/IBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder_Pattern
+{
+    interface IBuilder
+    {
+        void SetType();
+        void SetColor();
+        void SetEngine();
+        void SetModel();
+        void SetTopSpeed();
+        void SetAccessories();
+        Vehicle GetVehicle();
+    }
+}
